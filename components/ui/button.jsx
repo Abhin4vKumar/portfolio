8a68b265e8755aba6a7cacb82802691f31251566
Abhinav <abhinavkumar1312004@gmail.com>
2024-08-11 16:34:23 +0530

    Added Contact Page

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -15,7 +15,7 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-[44px] px-6",
-        sm: "h-[48px] px-6",
+        md: "h-[48px] px-6",
         lg: "h-[56px] px-8 text-sm uppercase tracking-[2px]",
       },
     },
