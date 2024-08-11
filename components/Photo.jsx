303fa0d34686ba447a722b0a263cae4e9804279d
Abhinav <abhinavkumar1312004@gmail.com>
2024-08-11 20:10:44 +0530

    Changed delay and duration of image

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -6,7 +6,7 @@ const Photo = () => {
   return (
     <div className="w-full h-full relative">
         <motion.div initial={{opacity:0}} animate={{opacity:1, transition:{delay:1,duration:0.4,ease:"easeIn"},}}>
-            <motion.div initial={{opacity:0}} animate={{opacity:1, transition:{delay:1.4,duration:0.4,ease:"easeInOut"},}} className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute">
+            <motion.div initial={{opacity:0}} animate={{opacity:1, transition:{delay:1.4,duration:0.7,ease:"easeInOut"},}} className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute">
                 <Image src="/assets/3.png" priority quality={100} fill alt="Abhinav Kumar" className="object-contain rounded-full" />
             </motion.div>
             <motion.svg className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]" fill="transparent" viewBox="0 0 506 506" xmlns="https://www.w3.org/2000/svg">
