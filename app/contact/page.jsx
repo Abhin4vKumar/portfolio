c122b133a0ff1cb0fb6519dbf1a22ac51d4416de
Abhinav <abhinavkumar1312004@gmail.com>
2024-08-11 16:37:56 +0530

    Escaped some entities

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -35,7 +35,7 @@ const Contact = () => {
           <div className="flex flex-col xl:flex-row gap-[30px]">
             <div className="xl:w-[54%] order-2 xl:order-none">
               <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-                <h3 className="text-4xl text-accent">Let's work together</h3>
+                <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
                 <p className="text-white/60">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                   <Input type="firstname" placeholder="Firstname" />
