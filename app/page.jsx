aac5a8f45071af7945dbd7bf4dcc61b0b6af37d1
Abhinav <abhinavkumar1312004@gmail.com>
2024-08-12 17:59:36 +0530

    Added CV

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import Photo from "@/components/Photo";
 import Socials from "@/components/Socials";
 import Stats from "@/components/Stats";
 import {Button} from "@/components/ui/button";
+import Link from "next/link";
 import {FiDownload} from "react-icons/fi";
 
 const Home = () => {
@@ -14,10 +15,12 @@ const Home = () => {
             <h1 className="h1 mb-6">Hello I&apos;m <br/> <span className="text-accent">Abhinav Kumar</span></h1>
             <p className="max-w-[500px] mb-9 text-white/80">I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies.</p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
-                <span>Download CV</span>
-                <FiDownload className="text-xl" />
-              </Button>
+              <Link target="_blank" href="https://drive.google.com/file/d/1FWJYMSvqcXAuejQlnS-vFRZTZ4pAmSCD/view">
+                <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                  <span>Download CV</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </Link>
               <div className="mb-8 xl:mb-0">
                 <Socials containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
               </div>
